aff111ba6d8da3a6be1bc4537dae77608e0aad9d
Raunak <raunakgiri21@gmail.com>
2023-09-12 19:37:57 +0530

    First Commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReview from "./CustomerReview";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export { Hero, PopularProducts, Services, SpecialOffer, CustomerReview, SuperQuality, Subscribe, Footer}
